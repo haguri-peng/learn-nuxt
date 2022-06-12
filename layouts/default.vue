b4e0da30cda736c11d8c3dc6a03aed2aa471b94e
haguri-peng <hty851111@naver.com>
2022-06-13 02:19:10 +0900

    add layouts

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header>
+      <h1>{{ capitalize }} Page</h1>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/main">Main</NuxtLink>
+      <NuxtLink to="/product">Product</NuxtLink>
+    </header>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    capitalize() {
+      const name = this.$route.name || '404'
+      return name.length > 0 ? name.charAt(0).toUpperCase() + name.slice(1) : ''
+    },
+  },
+}
+</script>
+
+<style></style>
