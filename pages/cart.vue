4334b13239fea4670dbc94214639e1a9fee5c87d
haguri-peng <hty851111@naver.com>
2022-06-18 10:40:15 +0900

    CartList Component 부분 삭제

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,34 +1,7 @@
 <template>
   <div class="container">
     <h1 class="list-title">Cart Page</h1>
-    <div class="list-wrapper">
-      <ul>
-        <li
-          v-for="cartItem in getCartItems"
-          :key="cartItem.id"
-          class="list-item"
-        >
-          <input type="checkbox" class="input-checkbox" />
-          <img
-            class="thumbnail"
-            :src="cartItem.imageUrl"
-            :alt="cartItem.name"
-          />
-          <div class="description">
-            <p>Name: {{ cartItem.name }}</p>
-            <p>Price: {{ cartItem.price }}</p>
-            <p>
-              Quantity:
-              <input
-                type="number"
-                class="input-number"
-                :value="cartItem.quantity || 1"
-              />
-            </p>
-          </div>
-        </li>
-      </ul>
-    </div>
+    <CartList></CartList>
     <div class="extra-panel">
       <button type="button">Purchase</button>
     </div>
@@ -36,15 +9,16 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+// import { FETCH_CART_ITEMS } from '@/store/index'
+import CartList from '@/components/CartList.vue'
 
 export default {
-  async fetch({ store }) {
-    await store.dispatch('getCartItems')
+  components: {
+    CartList,
   },
-  computed: {
-    ...mapGetters(['getCartItems']),
-  },
+  // async asyncData({ store }) {
+  //   await store.dispatch(FETCH_CART_ITEMS)
+  // },
 }
 </script>
 
@@ -56,29 +30,8 @@ export default {
   font-weight: 700;
   font-size: 1.4rem;
 }
-.list-wrapper {
-  margin: 0.4rem 0;
-}
-.list-item {
-  display: flex;
-  margin: 5px 0;
-}
-.thumbnail {
-  width: 150px;
-  height: 150px;
-}
-.description {
-  padding: 2rem 1rem;
-}
 .extra-panel {
   text-align: right;
   padding: 0.2rem 0;
-}
-.input-number {
-  width: 50px;
-  text-align: center;
-}
-.input-checkbox {
-  margin: 4rem 1rem 0 0;
 }
 </style>
